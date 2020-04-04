71b2675f1d679b0d1c3347620f2391a6341ce33a
Pedro Fernandez <pfernandez@payall.com.ve>
2020-04-04 12:44:24 -0400

    Deploy del ultimo commit

--- src/backend/vue-axios/axios.js
+++ src/backend/vue-axios/axios.js
@@ -4,8 +4,8 @@ import axios from "axios";
 import Vue from "vue";
 
 Vue.prototype.$http = axios;
-//const API_URL = "https://aquilesypedrotesis.host/wp-json"; //Produccion
-const API_URL = "http://172.23.0.3/wp-json";
+const API_URL = "https://aquilesypedrotesis.host/wp-json"; //Produccion
+//const API_URL = "http://172.23.0.3/wp-json";
 //const API_URL = process.env.URI_API_WORDPRESS;
 
 const token = localStorage.token;
